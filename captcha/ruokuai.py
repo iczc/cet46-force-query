e000ca5de211d458cdf906f8395d23b5446900d6
iczc <iczcalan@gmail.com>
2019-02-02 23:26:44 +0800

    update captcha api

--- captcha/ruokuai.py
+++ captcha/ruokuai.py
@@ -1,3 +1,4 @@
+import base64
 import requests
 
 from hashlib import md5
@@ -17,7 +18,9 @@ class RuokuaiClient:
         }
         
         self.headers = {
-            'Expect': '100-continue',
+            'Accept': '*/*',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'api.ruokuai.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
         }
 
@@ -29,13 +32,15 @@ class RuokuaiClient:
     def upload_image(self, image_path, typeid='3040', timeout=60):
         params = {
             'typeid' : typeid,
-            'timeout': timeout
+            'timeout': timeout,
+            'image': ''
         }
         params.update(self.base_params)
         with open(image_path, 'rb') as f:
             image = f.read()
-        files = {'image': ('captcha.png', image)}
-        response = requests.post('http://api.ruokuai.com/create.json', data=params, files=files, headers=self.headers)
+            base64_data = base64.b64encode(image)
+        params['image'] = base64_data
+        response = requests.post('http://api.ruokuai.com/create.json', data=params, headers=self.headers)
         return response.json()
 
     def report_error(self, image_id):
